e9d7afec9c575843fb52cfcbf877bb2641390bb6
Jennifer Techel <jennifertechel@hotmail.com>
2023-02-24 15:57:20 +0100

    component

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -3,7 +3,7 @@ import { createUseStyles } from "react-jss";
 function Card() {
   const classes = useStyles();
 
-  return <button></button>;
+  return <div></div>;
 }
 
 const useStyles = createUseStyles({});
