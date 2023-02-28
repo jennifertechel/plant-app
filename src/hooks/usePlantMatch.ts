44e72b157746751b736050aaaf97b622c81186a6
Jennifer Techel <jennifertechel@hotmail.com>
2023-02-28 13:35:48 +0100

    trying api

--- src/hooks/usePlantMatch.ts
+++ src/hooks/usePlantMatch.ts
@@ -1,20 +1,14 @@
 import { useEffect, useState } from "react";
 
-interface Plant {
-  Id: string;
-  Family: string;
-  Img: String;
-}
-
 export const usePlantMatch = () => {
-  const [plants, setPlants] = useState<Plant[]>([]);
-  //console.log(plants);
+  const [plants, setPlants] = useState([]);
+  console.log(plants);
 
   useEffect(() => {
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "87e81293d5mshba5ad19194785afp1215dejsn6fc7c39235a5",
+        // "X-RapidAPI-Key": "e8618a5b23mshf080c264a7ca9cdp1b2165jsn1f8716eb8c9e",
         "X-RapidAPI-Host": "house-plants2.p.rapidapi.com",
       },
     };
@@ -23,7 +17,7 @@ export const usePlantMatch = () => {
       .then((response) => response.json())
       .then((response) => setPlants(response))
       .catch((err) => console.error(err));
-  }, []);
+  });
 
   return plants.slice(0, 5);
 };
