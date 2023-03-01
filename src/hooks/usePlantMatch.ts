b3a54e60884136e7869c65450fb13efff9b56a8b
Jennifer Techel <jennifertechel@hotmail.com>
2023-03-01 10:29:31 +0100

    added filter plants for hook

--- src/hooks/usePlantMatch.ts
+++ src/hooks/usePlantMatch.ts
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import { useQuiz } from "../Components/QuizContext";
 
 interface Plant {
-  id: string;
-  ideallight: string;
+  family: string;
+  toleratedlight: string;
   watering: string;
 }
 
 export const usePlantMatch = () => {
   const [plants, setPlants] = useState<Plant[]>([]);
-  const { direction } = useQuiz();
+  const { direction, watering } = useQuiz();
 
   useEffect(() => {
     const options = {
@@ -26,8 +26,32 @@ export const usePlantMatch = () => {
       .catch((err) => console.error(err));
   }, []);
 
-  // filtrera plants baserat på direction & watering
+  // Filter plants based på direction and watering
+  const filteredPlants = plants.filter((plant) => {
+    const directionFilter =
+      (direction.north && plant.toleratedlight.includes("/")) ||
+      (direction.south && plant.toleratedlight.includes("Direct sunlight")) ||
+      (direction.east && plant.toleratedlight.includes("Diffused")) ||
+      (direction.west && plant.toleratedlight.includes("Direct sunlight"));
 
-  return plants.slice(0, 5);
+    const wateringFilter =
+      (watering.easy &&
+        plant.watering.includes(
+          "Water only when the soil is dry. Must be dry between watering"
+        )) ||
+      (watering.medium &&
+        plant.watering.includes(
+          "Water when soil is half dry. Can be dry between watering."
+        )) ||
+      (watering.hard &&
+        plant.watering.includes(
+          "Keep moist between watering. Water when soil is half dry."
+        ));
 
+    return directionFilter && wateringFilter;
+  });
+
+  console.log(filteredPlants);
+
+  return filteredPlants;
 };
